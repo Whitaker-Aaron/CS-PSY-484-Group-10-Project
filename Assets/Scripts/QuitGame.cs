19a92ab627f0826de37374facc1ff5e93a049405
LegendaryCorn <43460114+LegendaryCorn@users.noreply.github.com>
2024-05-10 20:10:21 -0700

    Main Menu in End Screen
    User can go to the main menu at the end screen.

--- Assets/Scripts/QuitGame.cs
+++ Assets/Scripts/QuitGame.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class QuitGame : MonoBehaviour
 {
+    public GameObject transition;
+
     public void doExitGame() 
     {
         Application.Quit();
+    }
+
+    public void goMainMenu()
+    {
+        transition.GetComponent<SceneTransitionManager>().GoToScene(0);
     }
 }
